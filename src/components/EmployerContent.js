dcbf3646aebe2a0bd7d90f10082cfaa2de242c4c
Jack Coombs <jack.coombs@hotmail.co.uk>
2022-07-07 20:49:45 +0100

    Added the airtable API.

--- src/components/EmployerContent.js
+++ src/components/EmployerContent.js
@@ -1,37 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import EmployerList from "./EmployerList";
 import EmployerFilters from "./EmployerFilters";
+import axios from "axios";
+import { SyncLoader } from "react-spinners";
 
 function EmployerContent() {
 
-  const [employerList, setemployerList] = useState([
-    {
-      "name": "BP",
-      "tags": ["FTSE"],
-      "maternityLeave": 26,
-      "paternityLeave": 13,
-      "pensionMatching": true,
-      "pensionContribution": 10,
-      "holidayEntitlement": 25
-    },
-    {
-      "name": "Vodafone",
-      "tags": ["FTSE", "TECH"],
-      "maternityLeave": 12,
-      "paternityLeave": 2,
-      "pensionMatching": true,
-      "pensionContribution": 6,
-      "holidayEntitlement": 30
+  axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.REACT_APP_API_KEY}`;
+  const [employerList, setEmployerList] = useState()
+
+  useEffect(() => {
+    const getCompanys = async () => {
+      const response = await axios.get(`https://api.airtable.com/v0/appkdJJNhilwa8hyn/Table%201?maxRecords=200&view=LIVE`)
+      const data = await response.data.records
+      const oldEmployerArray = await data.map(list => list.fields)
+      const newEmployerArray = await oldEmployerArray.map(employer => ({
+        name: employer["Name of Company"],
+        tags: employer["company type"],
+        maternityLeave: employer["Maternity Leave - Weeks at full pay?"],
+        paternityLeave: employer["Paternity - Weeks?"],
+        pensionMatching: employer["Pension - Matching?"],
+        pensionContribution: employer["Pension - Contributions"],
+        holidayEntitlement: employer["Balance - Holiday days"]
+      }))
+      setEmployerList(newEmployerArray)
     }
-  ])
+    getCompanys()
+  },[])
+
 
   return (
+
     <>
-      <EmployerFilters />
-      <EmployerList 
-        employerList={employerList}
-      />
+      { employerList ? 
+        <>
+          <EmployerFilters />
+          <EmployerList employerList={employerList}/>
+        </>
+      : <div className="loader">
+          <SyncLoader size="4rem" color="#091747"/>
+        </div>}
     </>
+    
   )
 }
 
