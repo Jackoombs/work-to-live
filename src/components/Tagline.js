fdbacb1605c9e8ba3881c34b1c552af104c5d765
Jack Coombs <jack.coombs@hotmail.co.uk>
2022-07-08 12:57:03 +0100

    Added emoji.

--- src/components/Tagline.js
+++ src/components/Tagline.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { FaPoo } from "react-icons/fa"
 
 function Tagline() {
   return (
     <section className="tagline">
       <h1>we can't all quit the day job</h1>
-      <h2>we're showing sh*t employers the door by ranking the UK's biggest employers on what matters</h2>
+      <h2>we're showing <FaPoo /> employers the door by ranking the UK's biggest employers on what matters</h2>
     </section>
   )
 }
